8a6608847d2cc960bfbe906338a72294a6f9939c
Yvette_Manishimwe <yvettemanishimwe9@gmail.com>
2025-06-18 15:44:20 +0200

    Adding carrer portal apis(inserjeune)

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -25,4 +25,21 @@ export class AuthService {
   async getDefaultAccessToken(): Promise<string> {
     return this.getAccessToken('andesanselme@gmail.com', 'Etrainer!132');
   }
+
+    async getCareerPortalAccessToken(email: string, password: string): Promise<string> {
+    const url = 'https://api.tvetcareerportal.rtb.gov.rw/api/v1/auth/login';
+    const body = { email, password };
+
+    try {
+      const response = await firstValueFrom(this.httpService.post(url, body));
+      return response.data.data.token; // Confirm this field name matches the API response
+    } catch (error) {
+      console.error('Career Portal Authentication failed:', error.response?.data || error.message);
+      throw error;
+    }
+  }
+
+  async getDefaultCareerPortalToken(): Promise<string> {
+    return this.getCareerPortalAccessToken('divineingabire69@gmail.com', 'Random@123');
+  }
 }
